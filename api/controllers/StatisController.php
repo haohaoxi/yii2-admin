d3fb5244ea7dfa55bb1bd3e0c8234bb27d9f05b3
haohaoxi <693310424@qq.com>
2018-11-26 14:21:56 +0800

    11/26

--- api/controllers/StatisController.php
+++ api/controllers/StatisController.php
@@ -10,10 +10,9 @@ use api\functionGlobal\FunctionRand;
 /**
  * StatisticsController implements the CRUD actions for Statistics model.
  */
+
 class StatisController extends  ActiveController
 {
-
-    public  $arri = array();
     public $modelClass = "";
     public function actions(){
         $action = parent::actions();
@@ -28,65 +27,64 @@ class StatisController extends  ActiveController
      * 参数 待定
      */
 
+    public function getmicrotime(){
+        return time();
+    }
+//redis 入队操作
     public function actionThestal()
     {
-
-        $redis = Yii::$app->redis;
-       $data = $_REQUEST;
-        $data = json_encode($data);
-
-        $in = $redis->rpush('queue',$data);
-//        echo $in;
-//        if($in){
-//            echo "入队成功";
-//        }
-
-        if($in>50){
-            $i = 0;
-            $vals  = "";
-            $t1 = microtime(true);
-        while ($value  = $redis->lpop('queue')) {
-            $i++;
-            $created_at = time();
-            $value = json_decode($value,true);
-            $connection = \Yii::$app->db;
-                $val = "({$value['admin_id']},'','','','','')";
-                $vals .= $val.',';
-        }
-                $content = substr($vals,0,-1);
-                $sql = "INSERT INTO yunmei_statistics (admin_id,admin_name,ip,created_at,phone_model,phone_size) values
-                $content";
-                $content  = $connection->createCommand("$sql")->execute();
-
-            $t2 = microtime(true);
-            $time =  round($t2-$t1,3);
-            echo $time;
-            $redis->rpush("qu",$time);
-        FunctionRand::View(1,'success','OK',1);
-        }
-        die;
-        echo "出队完成";
-//        $value = $redis->lpop('queue'); //出
-//        if($value){
-//            $value = json_decode($value,true);
-//        }else{
-//            echo "队列以空不在执行";
-//            die;
-//        }
-//        print_r($value);
-//        $redis->hmset('info','22','来不及挥手');
-//        $hash = $redis->hgetall('info');
-//        $redis->del("info");
-//        echo "<pre>";
-//        print_r($hash);
         if(Yii::$app->request->get()){
+            $redis = Yii::$app->redis;
+            if($_REQUEST){
+                $data = json_encode($_REQUEST);
+                $push = $redis->lpush("queue",$data);//入队 列表
+                $redis->set("list",$push);
+                echo $push;
+                if($push){
+                    echo "入队成功";
+                }
+               FunctionRand::View(1,'success','ok',1);
+            }else{
+                FunctionRand::Error('2','请求参数不正确');
+            }
 
-
-            FunctionRand::View(1,'success','OK',$content);
+            FunctionRand::View(1,'success','OK',1);
         }else{
             FunctionRand::Error('2','请求方式错误');
         }
 
+    }
+
+    public function actionThelpop(){
+        $redis = Yii::$app->redis;
+        var_dump($redis->get("list"));
+
+        if($redis->get("list")>10){
+            $vals = "";
+            $statime = microtime(true);
+        while($lpop = $redis->lpop("queue")){
+            $value = json_decode($lpop,true);
+            $connection = \Yii::$app->db;
+            $val = "({$value['admin_id']},'','','','','')";
+            $vals .= $val.',';
+        }
+        $content = substr($vals,0,-1);
+        $sql = "INSERT INTO yunmei_statistics (admin_id,admin_name,ip,created_at,phone_model,phone_size) values $content";
+        $content  = $connection->createCommand("$sql")->execute();
+        $endtiem = microtime(true);
+        $end = round($endtiem-$statime,3);
+        echo '结束时间减去开始时间:'.$end;
+            $redis->rpush("qu",$end);
+            echo 1111;$redis->del("list");
+            FunctionRand::View(3,'success','Nok','队列满足50条插入成功');
+
+            die;
+        }
+        else{
+            FunctionRand::View(3,'success','Nok','2');
+            die;
+        }
+        die;
     }
 
     /**
@@ -99,6 +97,8 @@ class StatisController extends  ActiveController
 
     }
 
+
+
     /**
      * Thestar
      * 页面注册接口统计
@@ -110,12 +110,6 @@ class StatisController extends  ActiveController
 
 
 
-
-
-
-
-
     }
-
 
 }
