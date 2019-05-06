2c0de49ad290801d45c4974551d89248d781f112
haohaoxi <693310424@qq.com>
2019-05-06 17:02:06 +0800

    2019.5.6

--- backend/controllers/AdverjsController.php
+++ backend/controllers/AdverjsController.php
@@ -96,9 +96,21 @@ class AdverjsController extends BaseController
         return $this->redirect(['index']);
     }
 
-    public function actionAjaxjs($content,$admin_id,$enum_id){
+    /**
+        $content  要生成url 链接 url-url-url
+     * $admin_id   用户的id
+     * $enum_id      广告位id
+     * $content_js   create 时为空 update为已经生成的<script></script> 形式的链接 用于在生成js链接判断
+      */
+    public function actionAjaxjs($content,$admin_id,$enum_id,$content_js){
         $addinfo =  Yii::$app->request->hostInfo;
+        if(!empty($content_js)){
+            $name =  substr($content_js,-19,-14);
+        }else{
+            $name = Func::getAbc("5");//获得js文件名称
+        }
         $addbase = Url::base();
+
         if(!empty($addbase)){
             $addurl = $addinfo.$addbase;
             $addinfobase = $addinfo.$addbase."/api/web";
@@ -111,13 +123,12 @@ class AdverjsController extends BaseController
            if(isset($content)){
                $arraylist = explode('-',$content);
                $pathadd = dirname(Yii::$app->basePath).$path;//js文件的绝对路径
-
                if(!is_dir($pathadd)){
                    mkdir($pathadd,777,true);
                    chmod($pathadd,777);
                }
                sleep(1);
-               $name = Func::getAbc("5");//获得js文件名称
+
                $text_js = Adverjs::wriTcontent($arraylist,$admin_id,$enum_id,$addurl,$addinfobase);
                $jsfile = fopen($pathadd.'/'.$name.'.js', "w"); //有就打开没有则创建的js文件名称
 
--- statics/themes/admin/views/adverjs/_form.php
+++ statics/themes/admin/views/adverjs/_form.php
@@ -20,9 +20,9 @@ $this->registerJsFile("/statics/themes/admin/js/jquery-2.2.4.min.js");
 
     <?= $form->field($model, 'enum_id')->dropDownList(Advertcontent::adtylenum_list($list = array())) ?>
 
-    <?= $form->field($model, 'content')->textarea(['roes'=>3]) ?>
+    <?= $form->field($model, 'content')->textarea(['roes'=>3])->hint("url填写方式：以-分割  如 url-url-url")->label() ?>
 
-    <?= $form->field($model, 'content_js')->textarea(['maxlength' => true,'rows'=>3]) ?>
+    <?= $form->field($model, 'content_js')->textarea(['maxlength' => true,'rows'=>3,'readonly'=>true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('添加', ['class' => 'btn btn-success']) ?>
@@ -38,17 +38,25 @@ $this->registerJsFile("/statics/themes/admin/js/jquery-2.2.4.min.js");
           var content = $("textarea[name='Adverjs[content]']").val();
           var admin_id = $("select[name='Adverjs[admin_id]']").val();
           var enum_id = $("select[name='Adverjs[enum_id]").val();
+           var content_js = $("textarea[name='Adverjs[content_js]']").val();
+           alert(content_js);
           if(content){
               $.ajax({
                   type:'GET',
                   url:"<?=  Url::to(['adverjs/ajaxjs']); ?>",
-                  data:{content:content,admin_id:admin_id,enum_id:enum_id},
+                  data:{content:content,admin_id:admin_id,enum_id:enum_id,content_js:content_js},
                   dataType:'',
                   success:function(data){
                       console.log(data);
                       // alert(data);
                       $("textarea[name='Adverjs[content_js]']").val(data);
+                      if(content_js==""){
+                          alert("js文件创建成功");
+                      }else{
+                          alert("js文件修改成功");
+                      }
                    },
+
                   error:function(jqXHR,textStatus,errorThrown){
                         alert("请求失败");
                       // alert(jqXHR.responseText);
